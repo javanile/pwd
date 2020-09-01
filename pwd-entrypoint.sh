eb82eaccf7ced8283c814dc4233146ce9e3d6d61
Francesco Bianco <info.francescobianco@gmail.com>
2020-09-01 18:01:24 +0200

    publish

--- pwd-entrypoint.sh
+++ pwd-entrypoint.sh
@@ -7,5 +7,5 @@ CONTAINER_ID=$(basename $(cat /proc/1/cpuset))
 #  chmod 777 pwd.yml
 #fi
 
-docker container inspect ${CONTAINER_ID} | jq -r '.[].Mounts[] | select(.Destination=="/pwd") | .Source' > pwd.var
-chmod 777 pwd.var
+docker container inspect ${CONTAINER_ID} | jq -r '.[].Mounts[] | select(.Destination=="/pwd") | .Source'
+#chmod 777 pwd.var
